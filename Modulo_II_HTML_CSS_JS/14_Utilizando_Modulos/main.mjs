923e305c8b3ad96a720b29f4d39ce6e6940da5f4
rianmiranda <rianmiranda1@gmail.com>
2022-07-28 12:12:09 -0300

    Adiciona os arquivos produzidos no curso de Módulos em Javascript.

---
+++ Modulo_II_HTML_CSS_JS/14_Utilizando_Modulos/main.mjs
@@ -0,0 +1,6 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraCidade("Pedrinho", "Long Beach, CA"))
+
+console.log(mostraIdade("Pedrinho", 11))
+console.log(mostraHobby("Pedrinho", "passear em downtown e visitar a Queen Mary"))
