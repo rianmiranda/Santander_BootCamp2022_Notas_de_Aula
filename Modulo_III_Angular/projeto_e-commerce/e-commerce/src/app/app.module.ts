5e9e1e8adffb4f245fb6d3e58d018ab79fd1f34b
rianmiranda <rianmiranda1@gmail.com>
2022-08-08 11:16:52 -0300

    Adiciona os arquivos do projeto e-commerce.

---
+++ Modulo_III_Angular/projeto_e-commerce/e-commerce/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ECommerceAppComponent } from './components/e-commerce-app/e-commerce-app.component';
+import { FiltersComponent } from './components/e-commerce-app/filters/filters.component';
+import { ProductListComponent } from './components/e-commerce-app/product-list/product-list.component';
+import { ProductItemComponent } from './components/e-commerce-app/product-list/product-item/product-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ECommerceAppComponent,
+    FiltersComponent,
+    ProductListComponent,
+    ProductItemComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Modulo_III_Angular/projeto_e-commerce/e-commerce/src/app/components/e-commerce-app/product-list/product-list.component.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { from } from "rxjs";
+import { HttpClientModule } from '@angular/common/http';
+import { Book } from "./model/Book";
+
+@Injectable()
+
+export class BookService {
+
+
+}
